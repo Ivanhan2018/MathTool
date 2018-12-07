a2feb8578d61694ddc78b5daee651c6f45d05e4e
Ivanhan2018 <hanxiaohua85@163.com>
2018-12-07 20:57:42 +0800

    计算有限群的最小生成元集、秩

--- gaptool/GroupUtil.h
+++ gaptool/GroupUtil.h
@@ -1,12 +1,16 @@
 ﻿#ifndef _GROUP_UTIL_H
 #define _GROUP_UTIL_H
 
+#include <cstdio>
+#include <cmath>
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 using namespace std;
 
 class GroupUtil
@@ -23,6 +27,36 @@ public:
 
    // 有限生成置换群的小工具FG.exe
    static bool FG(const char *szFG,const char *szfilename);
+   
+   // 群的最小生成元集、秩(eg:rank(Q_8)=2,rank(D_4)=2)
+   static vector<int> rank(const vector<vector<int> > &A);
+
+   static vector<char> lof(const char *fn);
+
+   static vector<char> CharArrToNormal(const vector<char>& vec);
+
+   static string CharArrToStr(const vector<char>& vec);
+
+   static vector<vector<int> > atoTable(const char* strMtx);
+
+   static vector<int> IsLegalMtx(const vector<vector<int> > &mtx);
+
+   static vector<vector<int> > ABmul(const vector<vector<int> > &A,const vector<vector<int> > &B);
+
+   static bool SaveTable(const char *fn,const vector<vector<int> > &A);
+
+   static vector<int>  Factors(int n);
+
+   // 未知n阶群的群元的阶
+   static int getGnEOrder(const vector<vector<int> > &A,int a);
+
+   static bool SaveGnEOrder(const char *srcfn,const char *Desfn,const char *DesGn=0);
+
+   // 从路径名中分离出文件名
+   static char * find_file_name(char *name);
+
+   // 过滤掉文件名中的扩展名
+   static void filter_ext_name(char *name);
 };
 
 #endif
