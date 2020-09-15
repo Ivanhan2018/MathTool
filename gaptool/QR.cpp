5ed769bb661f3fb07dab283495abc0ac50ea7fef
HEMEI-20180102W\Administrator <hanxiaohua85@163.com>
2020-09-15 23:24:48 +0800

    27阶环已找到57种，还剩2种未找到

--- gaptool/QR.cpp
+++ gaptool/QR.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]){
 #ifdef IDR		
 		printf("Usage:  IDR n1 n2 [M2]\n");
 #else
-		printf("Usage:  IDR n1 n2 [M2]\n");
+		printf("Usage:  QR n1 n2 [M2]\n");
 #endif	
 		return 0;
 	}
@@ -17,13 +17,16 @@ int main(int argc, char* argv[]){
 		str=argv[3];
 	char* str1="";
     if(argc>4)	
-		str1=argv[4];  	
+		str1=argv[4];  
+    char* str0="R32_";
+    if(argc>5)	
+		str0=argv[5];	
 	for(int i=n1;i<=n2;i++)	{
 		char sz[1000]={0};
 #ifdef IDR
-		sprintf(sz,"IdRing R32_%d%s.txt",i,str);
+		sprintf(sz,"IdRing %s%d%s.txt",str0,i,str);
 #else
-		sprintf(sz,"QR16 R32_%d%s.txt %s",i,str,str1);
+		sprintf(sz,"QR16 %s%d%s.txt %s",str0,i,str,str1);
 #endif
 		printf("i=%d\n%s\n",i,sz);
 		system(sz);
